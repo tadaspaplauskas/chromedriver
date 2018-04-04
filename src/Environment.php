eba782bdbdca3695fae2ead3063685e00be4a104
Tadas <tadaspaplauskas@gmail.com>
2018-04-04 11:22:55 +0300

    correct windows filename

--- src/Environment.php
+++ src/Environment.php
@@ -7,7 +7,7 @@ class Environment
     public static function set()
     {
         if (PHP_OS === 'WINNT' || strpos(php_uname(), 'Microsoft') !== false) {
-            $driver = 'chromedriver-win';
+            $driver = 'chromedriver-win.exe';
         }
         else if (PHP_OS === 'Darwin') {
             $driver = 'chromedriver-mac';
